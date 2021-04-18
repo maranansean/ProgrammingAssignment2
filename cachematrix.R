b0bebba3f2eb40e12e6eb46ee7cf67d8b03f792e
maranansean <82748176+maranansean@users.noreply.github.com>
2021-04-18 15:16:01 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+makeCache Matrix <- function(x = matrix(sample(1:100,9),3,3)){
+        s <- NULL
+        set <-function(y) {
+                x <<- y
+                s <<- NULL
 }
+        get <- function() x
+        setmean <-function(amount) s <<- amount
+        getmean <- function() m
+        list(set = set, 
+             get = get
+        setamount = setamount,
+        getamount = getamount)
+        }
+  
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+     
         ## Return a matrix that is the inverse of 'x'
+        cacheamount <- function(x, ...) {
+                s <- x$getamount()
+                if(!is.null(s)) {
+                        message("solving for the inversed matrix")
+                        return(s)
+                        }
+                data <- x$get()
+                s <-amount(data, ...)
+                x$setamount
+                s
+                
 }
